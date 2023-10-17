383160c9a61bafaac9f50014264a23c9a060807c
UNI\up2120141 <up2120141@myport.ac.uk>
2023-10-17 10:57:11 +0100

    First commit for other version

---
+++ Source/RacingAPIGameUnreal/RacingAPIGameUnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RacingAPIGameUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RACINGAPIGAMEUNREAL_API ARacingAPIGameUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
